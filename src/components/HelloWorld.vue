57178d992c3afa12e2ef057ddc62352b76aedfa9
Gaein nidb <mail@gaein.cn>
2023-09-24 01:30:20 +0800

    init page

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container :fluid="true" class="fill-height">
+    <v-carousel hide-delimitersi>
+      <v-carousel-item
+        src="/banners/01.webp"
+
+      ></v-carousel-item>
+
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+        cover
+      ></v-carousel-item>
+
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        cover
+      ></v-carousel-item>
+    </v-carousel>
+
+  </v-container>
+</template>
+
+<script setup>
+//
+</script>
