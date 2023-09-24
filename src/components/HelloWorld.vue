cdb28168018b4d9e4c7a6ef1b30e23e0d61a8f84
Gaein nidb <mail@gaein.cn>
2023-09-24 20:18:46 +0800

    update index page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,17 @@
 <template>
-  <v-container :fluid="true" class="fill-height">
+  <v-container :fluid="true">
     <v-card>
       <v-card-title>
-        鳝鱼的南桐心 | うなぎのゲイ心
+        鳝鱼は？
       </v-card-title>
+      <v-card-text>
+        刘善宇，山东枣庄人也，生性嗜好同行，世人谓之曰gay。
+        <v-carousel hide-delimitersi>
+          <v-carousel-item src="/banners/01.webp"></v-carousel-item>
+          <v-carousel-item src="/banners/02.webp"></v-carousel-item>
+        </v-carousel>
+      </v-card-text>
     </v-card>
-    <v-carousel hide-delimitersi>
-      <v-carousel-item
-        src="/banners/01.webp"
-
-      ></v-carousel-item>
-
-      <v-carousel-item
-        src="/banners/02.webp"
-
-      ></v-carousel-item>
-    </v-carousel>
-
   </v-container>
 </template>
 
--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,12 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
+      <v-icon icon="mdi-circle-slice-4"/>
+      鳝鱼的南桐心 | うなぎのゲイ心
     </v-app-bar-title>
   </v-app-bar>
 </template>
 
 <script setup>
-  //
+//
 </script>
